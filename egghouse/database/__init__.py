aab9dc9274e76c506689465e7740e96d49fe8752
Eunsu Park <phd.choux@gmail.com>
2026-05-12 11:35:32 +0900

    Bump version to 0.2.0

--- egghouse/database/__init__.py
+++ egghouse/database/__init__.py
@@ -8,4 +8,4 @@ from .postgres import PostgresManager, to_dataframe
 from .config import load_config, from_dict, create_example_config
 
 __all__ = ['PostgresManager', 'to_dataframe', 'load_config', 'from_dict', 'create_example_config']
-__version__ = '0.1.0'
+__version__ = '0.2.0'
